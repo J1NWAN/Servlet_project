b96e2b223c64589c0bfe9b7ea0c70309d7099bfb
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2021-05-25 11:16:37 +0900

    로또 생성 구현

---
+++ src/com/servlet/project/Lotto.java
@@ -0,0 +1,44 @@
+package com.servlet.project;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/lotto")
+public class Lotto extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		int value = Integer.parseInt(request.getParameter("num"));
+
+		int[] num = new int[6];
+		ArrayList<String> list = new ArrayList<String>();
+
+		for (int i = 0; i < value; i++) {
+			for (int j = 0; j < num.length; j++)
+				num[j] = (int)(Math.random() * 46 + 1);
+			
+			list.add(num[0] + " " + num[1] + " " + num[2] + " " + num[3] + " " + num[4] + " " + num[5]);
+		}
+
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>로또 번호</TITLE></HEAD>");
+		out.println("<body><center>");
+		out.println("<h2>생성 결과</h2><hr>");
+		for (int i = 0; i < list.size(); i++) {
+			out.println(i+1 + "회차 : " + list.get(i));
+			out.println("<br><br>");
+		}
+		out.println("</body></html>");
+	}
+}
